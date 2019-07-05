5ff113002451e9991ecaa0a3c0b30986ce5ccf08
guolin <1758294957@qq.com>
2019-07-05 15:55:37 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "element-ui/lib/theme-chalk/index.css";
+import ElementUI from "element-ui";
+
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
   router,
