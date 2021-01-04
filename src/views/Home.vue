b000a2bc2abef488d368b36f8f77cc1cc2556956
weiTangshi <w1452237060@163.com>
2021-01-04 15:30:54 +0800

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,27 +2,28 @@
   <div class="threeLevelMain">
     <!-- 报价容器 -->
     <div class="quoteContainer">
+      <img :src="form.imgUrl" alt="" />
       <!-- 报价顶部信息 -->
       <div class="quote_info clearfix">
         <h3 class="h3_title">报价单</h3>
         <div class="quote_itemBox">
           <div class="quote_item">
             <span class="quote_label">客户名称：</span>
-            <p class="quote_p">{{form.custName}}</p>
+            <p class="quote_p">{{ form.custName }}</p>
           </div>
           <div class="quote_item">
             <span class="quote_label">联系方式：</span>
-            <p class="quote_p">{{form.phoneNumber}}</p>
+            <p class="quote_p">{{ form.phoneNumber }}</p>
           </div>
         </div>
         <div class="quote_itemBox">
           <div class="quote_item">
             <span class="quote_label">项目要求：</span>
-            <p class="quote_p">{{form.projectRequirement}}</p>
+            <p class="quote_p">{{ form.projectRequirement }}</p>
           </div>
           <div class="quote_item">
             <span class="quote_label">备注：</span>
-            <p class="quote_p">{{form.remark}}</p>
+            <p class="quote_p">{{ form.remark }}</p>
           </div>
         </div>
       </div>
@@ -36,16 +37,41 @@
           :data="tableData"
           style="width: 100%"
         >
-          <el-table-column prop="number" width="80" label="序号" align="center"></el-table-column>
-          <el-table-column label="设备名称" prop="name" align="center"></el-table-column>
-          <el-table-column label="数量" prop="num" align="center"></el-table-column>
-          <el-table-column prop="salePrice" label="销售单价" align="center"></el-table-column>
-          <el-table-column prop="saleTotal" label="销售合计" align="center"></el-table-column>
-          <el-table-column label="备注" prop="remark" align="center"></el-table-column>
+          <el-table-column
+            prop="number"
+            width="80"
+            label="序号"
+            align="center"
+          ></el-table-column>
+          <el-table-column
+            label="设备名称"
+            prop="name"
+            align="center"
+          ></el-table-column>
+          <el-table-column
+            label="数量"
+            prop="num"
+            align="center"
+          ></el-table-column>
+          <el-table-column
+            prop="salePrice"
+            label="销售单价"
+            align="center"
+          ></el-table-column>
+          <el-table-column
+            prop="saleTotal"
+            label="销售合计"
+            align="center"
+          ></el-table-column>
+          <el-table-column
+            label="备注"
+            prop="remark"
+            align="center"
+          ></el-table-column>
           <div slot="append">
             <div class="quoteTable">
               <span class="quoteTable_sp1">合计：</span>
-              <span class="quoteTable_sp1">{{form.totalPrice}}</span>
+              <span class="quoteTable_sp1">{{ form.totalPrice }}</span>
             </div>
           </div>
         </el-table>
@@ -56,21 +82,28 @@
       <div class="title_checkReason">审核备注：</div>
       <div class="checkReasonMain">
         <div class="p_box">
-          <p class="p_checkReason">{{form.checkReason}}</p>
+          <p class="p_checkReason">{{ form.checkReason }}</p>
         </div>
       </div>
     </div>
     <!-- 底部按钮容器 -->
     <div class="botmBtnContainer">
-      <el-button @click="exportWord" size="small" type="primary">导出word</el-button>
+      <el-button @click="exportWord" size="small" type="primary"
+        >导出word</el-button
+      >
       <!-- <el-button @click="exportExcelClick" size="small" type="primary">导出excel</el-button> -->
     </div>
   </div>
 </template>
 <script>
+import JSZipUtils from 'jszip-utils'
+import docxtemplater from 'docxtemplater'
+import { saveAs } from 'file-saver'
+import PizZip from 'pizzip'
+
 export default {
   name: "home",
-  data() {
+  data () {
     return {
       // 表单对象
       form: {
@@ -78,14 +111,17 @@ export default {
         phoneNumber: "138xxxxxxxx", // 联系方式
         projectRequirement: "为了更美好的明天而战", // 项目要求
         totalPrice: 140, // 合计报价
+        imgUrl: require('../assets/logo.jpg'),
         remark: "QEWARAEQAAAAAAAAA", // 备注
         checkReason: '同意' // 审核备注
+
       },
+
       // 表格信息
       tableData: []
     };
   },
-  created() {
+  created () {
     this.tableData = [
       {
         number: 1, // 序号
@@ -113,64 +149,109 @@ export default {
       }
     ];
   },
+  mounted () { this.ImageToBase64() },
   methods: {
-    // 点击导出word
-    exportWord: function() {
-      let _this = this;
-      // 读取并获得模板文件的二进制内容
-      JSZipUtils.getBinaryContent("input.docx", function(error, content) {
-        // input.docx是模板。我们在导出的时候，会根据此模板来导出对应的数据
-        // 抛出异常
+    ImageToBase64 () {
+      let image = new Image();
+      let imageUrl = 'http://localhost:8080/img/logo.82b9c7a5.jpg'
+      image.src = imageUrl
+      let that = this;
+      image.onload = () => {
+        var canvas = document.createElement("canvas")
+        canvas.width = image.width;
+        canvas.height = image.height;
+        var context = canvas.getContext('2d');
+        context.drawImage(image, 0, 0, image.width, image.height);
+        var quality = 0.8;
+        //这里的dataurl就是base64类型
+        var dataURL = canvas.toDataURL("image/png", quality);//注意图片格式一定要是png的
+        console.log(dataURL);
+        this.form.imgUrl = dataURL
+      }
+    },
+    // 
+    //这里是官网写的应该是处理图片的代码，照着写就行，没动过
+    base64DataURLToArrayBuffer (dataURL) {
+      const base64Regex = /^data:image\/(png|jpg|svg|svg\+xml);base64,/;
+      if (!base64Regex.test(dataURL)) {
+        return false;
+      }
+      const stringBase64 = dataURL.replace(base64Regex, "");
+      let binaryString;
+      if (typeof window !== "undefined") {
+        binaryString = window.atob(stringBase64);
+      } else {
+        binaryString = new Buffer(stringBase64, "base64").toString("binary");
+      }
+      const len = binaryString.length;
+      const bytes = new Uint8Array(len);
+      for (let i = 0; i < len; i++) {
+        const ascii = binaryString.charCodeAt(i);
+        bytes[i] = ascii;
+      }
+      return bytes.buffer;
+    },
+    // 导出word
+    exportWord () {
+      //这里要引入处理图片的插件，下载docxtemplater后，引入的就在其中了
+      var ImageModule = require('docxtemplater-image-module-free');
+      var that = this;
+      //这里是我的Word路径，在static文件下
+      JSZipUtils.getBinaryContent("input.docx", function (error, content) {
         if (error) {
-          throw error;
+          throw error
+        };
+        let opts = {}
+        opts.centered = true;
+        opts.fileType = "docx";
+        opts.getImage = (tag) => {
+          return that.base64DataURLToArrayBuffer(tag);
         }
-        
-        // 创建一个JSZip实例，内容为模板的内容
-        let zip = new JSZip(content);
-        // 创建并加载docxtemplater实例对象
-        let doc = new window.docxtemplater().loadZip(zip);
-        // 设置模板变量的值
+        opts.getSize = () => {
+          return [600, 400]//这里可更改输出的图片宽和高
+        }
+        let zip = new PizZip(content);
+        let doc = new docxtemplater();
+        doc.attachModule(new ImageModule(opts));
+        doc.loadZip(zip);
+        console.log({});
         doc.setData({
-          ..._this.form,
-          table: _this.tableData
+          ...that.form,
+          table: that.tableData//我的最外层包裹一切要导出的数据名称
         });
-        
         try {
-          // 用模板变量的值替换所有模板变量
-          doc.render();
+          doc.render()
         } catch (error) {
-          // 抛出异常
-          let e = {
+          var e = {
             message: error.message,
             name: error.name,
             stack: error.stack,
-            properties: error.properties
-          };
-          console.log(JSON.stringify({ error: e }));
+            properties: error.properties,
+          }
+          console.log(JSON.stringify({
+            error: e
+          }));
           throw error;
         }
-        
-        // 生成一个代表docxtemplater对象的zip文件（不是一个真实的文件，而是在内存中的表示）
-        let out = doc.getZip().generate({
+        var out = doc.getZip().generate({
           type: "blob",
-          mimeType:
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-        });
-        // 将目标文件对象保存为目标类型的文件，并命名
-        saveAs(out, "报价单.docx");
-      });
-    }
+          mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        })
+        saveAs(out, "成绩报表.docx")
+      })
+    },
+
   }
 };
 </script>
 <style lang="scss">
 * {
-	margin: 0;
-	padding: 0;
-	font-size: 12px;
-	font-family: "微软雅黑", "宋体";
-	list-style: none;
-	box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-size: 12px;
+  font-family: "微软雅黑", "宋体";
+  list-style: none;
+  box-sizing: border-box;
 }
 // 清除浮动
 .clearfix:after {
